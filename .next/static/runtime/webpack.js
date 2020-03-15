4904784230fbc191f4198cec31ced3f1ba8e06dd
Nitin Reddy <reddynitin3@gmail.com>
2020-03-15 22:16:48 +0530

    Added static pages

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6000156d757cf0ae4de9";
+/******/ 	var hotCurrentHash = "57a7aebfdf0a334d1cb5";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
